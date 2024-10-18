26e2879b1a244a70a9725ecc14dad84c370d3b5b
vdichev26 <viko.dichev@gmail.com>
2024-10-18 10:45:07 +0300

    Improved Guessing game and file system

---
+++ 25_10_2024/src/ImprovedGuessing.java
@@ -0,0 +1,38 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class ImprovedGuessing {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random rn = new Random();
+        int num = rn.nextInt(100) + 1;
+        int attempts = 0;
+        System.out.println("Welcome to the guessing game. You'll need to guess the correct number between 1 and 100. You can always quit by typing \"quit\". Good luck!");
+        int answer;
+        do {
+            if (sc.hasNextInt()) {
+                answer = sc.nextInt();
+                if (answer > num) {
+                    System.out.println("The number is lower than the number you guessed.");
+                } else if (answer < num) {
+                    System.out.println("The number is greater than the number you guessed.");
+                } else {
+                    System.out.println("You guessed correctly!");
+                    break;
+                }
+            } else {
+                String text = sc.next();
+                if(text.equals("quit")){
+                    System.out.println("Thank you for playing. The correct number was " + num + " Goodbye!");
+                    break;
+                }else{
+                    System.out.println("Invalid input.");
+                }
+            }
+            attempts++;
+        } while (true);
+        System.out.println("You made " + attempts + " attempts.");
+        sc.close();
+    }
+}
