d3f4a5ca4ab95760a2af2d1e946769e2b1cd4c0e
vdichev26 <viko.dichev@gmail.com>
2025-01-15 09:39:39 +0200

    Made Rock, Paper, Scissors title screen (early stages)

---
+++ 21_01_2025/src/RockPaperScissors.java
@@ -0,0 +1,51 @@
+import java.awt.*;
+import javax.swing.*;
+
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+
+public class RockPaperScissors {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Rock Paper Scissors");
+        frame.setSize(1200, 900);
+        frame.setResizable(true);
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setLayout(null);
+
+        JLabel titleLabel = new JLabel("Rock, Paper, Scissors");
+        titleLabel.setFont(new Font("Algerian", Font.BOLD | Font.ITALIC, 45));
+        titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        titleLabel.setBounds(350, 200, 500, 100);
+
+        JButton startGameButton = new JButton("Start Game");
+        buttonStyle(startGameButton);
+        startGameButton.setBounds(450, 400, 300, 60);
+
+        JButton exitGameButton = new JButton("Exit Game");
+        buttonStyle(exitGameButton);
+        exitGameButton.setBounds(450, 500, 300, 60);
+
+        JLabel nameLabel = new JLabel("Made by Victor Dichev ;)");
+        nameLabel.setFont(new Font("Algerian", Font.BOLD | Font.ITALIC, 22));
+        nameLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        nameLabel.setBounds(450, 800, 300, 40);
+
+        //SplashScreen splash = SplashScreen.getSplashScreen();
+        //Graphics2D g2d = splash.createGraphics();
+
+
+        frame.add(titleLabel);
+        frame.add(startGameButton);
+        frame.add(exitGameButton);
+        frame.add(nameLabel);
+        frame.getContentPane().setBackground(Color.decode("#ADEEE3"));
+        //frame.add(splash);
+        frame.setVisible(true);
+    }
+
+    public static void buttonStyle(JButton button) {
+        button.setFont(new Font("Arial", Font.BOLD, 32));
+        button.setForeground(Color.BLUE);
+        button.setFocusPainted(false);
+    }
+}
